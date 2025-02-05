b5900f1357e94b7259a8199a0892e613185f1647
mmen88499 <momen88499@gmail.com>
2025-02-05 10:27:59 +0200

    start programing with js , html , css

---
+++ index.js
@@ -0,0 +1,7 @@
+let fullname ="Mohamed Medhat ";
+let age =22;
+let student = true ;
+
+document.getElementById("p1").textContent = `My Nmae Is ${fullname}`
+document.getElementById("p2").textContent = `My Age Is ${age} Years Old`;
+document.getElementById("p3").textContent = `Iam Student ? ${student} !`;
